cd4eaadfbc29962f776e8c00ce6f29badde36c03
Igor Vergas <igor_vergani@hotmail.com>
2023-09-14 07:33:51 -0300

    Add project files.

---
+++ CursoC#/CursoC#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+
+            Console.WriteLine("Digite um numero:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int soma = n1 + n2;
+
+            Console.WriteLine("A soma dos números digitados é: " + soma);
+
+
+
+        }
+
+    }
+
+
+}
+
