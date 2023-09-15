10cde887f48cb93074bffe88872b54554d467440
Igor Vergas <igor_vergani@hotmail.com>
2023-09-15 08:00:16 -0300

    add ex2

--- CursoC#/CursoC#/Program.cs
+++ CursoC#/CursoC#/Program.cs
@@ -7,17 +7,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            //Exercicio 1
-
-            Console.WriteLine("Digite um numero:");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite outro numero");
-            int n2 = int.Parse(Console.ReadLine());
-
-            int soma = n1 + n2;
-
-            Console.WriteLine("A soma dos números digitados é: " + soma);
-
+           
 
 
         }
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 2
+
+            
+
+            Console.WriteLine("Digite o valor do raio:");
+            double raio = double.Parse(Console.ReadLine());
+
+            double area = 3.14159 * raio * raio;
+
+            Console.WriteLine("O valor da área é :" +  area.ToString());
+
+
+
+
+        }
+
+    }
+
+
+}
+
